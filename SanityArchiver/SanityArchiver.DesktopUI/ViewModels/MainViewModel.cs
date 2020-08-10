4ac1ddf74c121add3ba73e845e4afd6da95d0acb
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2020-08-10 16:03:50 +0200

    Basic TreeView is implemented

---
+++ SanityArchiver/SanityArchiver.DesktopUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SanityArchiver.Application.Models;
+
+namespace SanityArchiver.DesktopUI.ViewModels
+{
+    /// <summary>
+    /// MainViewModel dolog Class
+    /// </summary>
+    public class MainViewModel
+    {
+        /// <summary>
+        /// Instance of DriveExplorer
+        /// </summary>
+        public DriveExplorer _driveExplorer;
+
+        /// <summary>
+        /// List of Directories
+        /// </summary>
+        private ObservableCollection<MenuItem> _menuItems = new ObservableCollection<MenuItem>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+            _driveExplorer = new DriveExplorer();
+        }
+
+        /// <summary>
+        /// Gets menuItems Property
+        /// </summary>
+        public ObservableCollection<MenuItem> MenuItems
+        {
+            get
+            {
+                return _menuItems;
+            }
+        }
+
+        /// <summary>
+        /// Fills Up the MenuItems with the local drives
+        /// </summary>
+        public void FillUpMenuItems()
+        {
+            MenuItem root = new MenuItem { Title = "Local Drive" };
+            DirectoryInfo driveE = new DirectoryInfo(@"E:\");
+            foreach (MenuItem item in _driveExplorer.WalkLocalDrive(driveE, root))
+            {
+                root.Items.Add(item);
+            }
+
+            _menuItems.Add(root);
+            }
+        }
+    }
--- SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
+++ SanityArchiver/SanityArchiver.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using SanityArchiver.Application.Models;
+using SanityArchiver.DesktopUI.ViewModels;
 
 namespace SanityArchiver.DesktopUI.Views
 {
@@ -10,9 +14,23 @@ namespace SanityArchiver.DesktopUI.Views
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
+        private MainViewModel _vm;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            _vm = new MainViewModel();
+            DataContext = _vm;
+            _vm.FillUpMenuItems();
+            treeViewMenu.Items.Add(_vm.MenuItems);
+        }
+
+        private void TreeViewMenu_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.Source as TreeViewItem;
         }
     }
 }
